114e40c123ccab9ea2b729e8f201c6c034cc1ab7
adag21044 <adag21044@gmail.com>
2025-09-22 09:25:31 +0300

    Update PauseManager.cs

--- Assets/_Project/Scripts/Runtime/GameManager/PauseManager.cs
+++ Assets/_Project/Scripts/Runtime/GameManager/PauseManager.cs
@@ -5,7 +5,7 @@ public class PauseManager : MonoBehaviour
     [SerializeField] private GameObject pausePanel;
     public bool isPaused = false;
 
-    
+
 
     public void TogglePause()
     {
@@ -21,18 +21,23 @@ public class PauseManager : MonoBehaviour
         isPaused = true;
         pausePanel.SetActive(false); // sadece menü görünsün, pause menüsü değil
     }
-    
+
     public void PauseGame()
     {
         pausePanel.SetActive(true);
         Time.timeScale = 0f; // pause
         isPaused = true;
     }
-    
+
     public void ResumeGame()
     {
         pausePanel.SetActive(false);
         Time.timeScale = 1f; // resume
         isPaused = false;
+    }
+
+    public void StopPlaying()
+    {
+        Time.timeScale = 0f;
     }
 }
